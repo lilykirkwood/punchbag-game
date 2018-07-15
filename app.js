757ee68b5c1a697b959bdaa759191a4a7287953e
Lily Kirkwood <lily.kirkwood@humanstate.com>
2018-07-15 13:28:38 +0100

    punch method, restart method, vue file fixes, vue integration to index.html

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 new Vue({
-  el: 'app',
+  el: '#app',
   data: {
-
+    health: 100,
+    ended: false
   },
-  methods {
-
+  methods: {
+    punch: function() {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      };
+    },
+    restart: function() {
+      this.health = 100;
+      this.ended = false;
+    }
   },
-  computed {
-    
+  computed: {
+
   }
 });
